88edbd5e969bdddf914366fcf2c60012ff0f6d9b
Александр Пашнин <panikun@yandex.ru>
2015-11-22 20:35:11 +0300

    laba 5. Reviewed Oracle Example. Made try-with-resourses. It works, but need make it more pretty and stable

--- laba5Client/src/RequestSender.java
+++ laba5Client/src/RequestSender.java
@@ -1,15 +1,18 @@
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Created by panikun on 22.11.15.
  */
 public class RequestSender implements Runnable {
     public static final int PORT=11111;
-    Socket socket;
+    //Socket socket;
     float r;
     float x;
     float y;
@@ -21,46 +24,68 @@ public class RequestSender implements Runnable {
 
     }
 
-   private void closeSocket() {
-        try {
-            this.socket.close();
-        } catch (IOException e) {
-        }
-    }
-
     @Override
     public void run() {
         this.y = (float)point.Y;
         this.x = (float)point.X;
         PrintStream ps;
         InputStreamReader ir;
-        try {
-            socket = new Socket("localhost", RequestSender.PORT);
-            ps = new PrintStream(socket.getOutputStream());
-            ir = new InputStreamReader(socket.getInputStream());
-        } catch (IOException e) {
-            System.err.println("Не удалось отправить запрос: ");
-            point.setGreyColor();
-            return;
-        }
-        ps.print(x + ";" + y + ";" + r);
-
-        ps.close();
-        BufferedReader br = new BufferedReader(ir);
         String message;
-        try {
-            message = br.readLine();
+        try(
+                Socket socket = new Socket("localhost", RequestSender.PORT);
+                PrintStream pr= new PrintStream(socket.getOutputStream());
+                BufferedReader brdr=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        ) {
+            pr.println(x + ";" + y + ";" + r);
+            message=brdr.readLine();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            point.setGreyColor();
+            return;
         } catch (IOException e) {
             point.setGreyColor();
-            System.err.println("Why?");
+            e.printStackTrace();
             return;
         }
-        System.out.print(message);
+
+
+//        try {
+//            socket = new Socket("localhost", RequestSender.PORT);
+//            ps = new PrintStream(socket.getOutputStream());
+//        } catch (IOException e) {
+//            System.err.println("Не удалось отправить запрос: ");
+//            point.setGreyColor();
+//            return;
+//        }
+       // pr.println(x + ";" + y + ";" + r);
+       // ps.flush();
+
+
+
+//        try {
+//            ir = new InputStreamReader(socket.getInputStream());
+//            BufferedReader br = new BufferedReader(ir);
+//            message = br.readLine();
+//        } catch (IOException e) {
+//            point.setGreyColor();
+//            System.err.println(e);
+//            return;
+//        }
+        
         switch (message){
             case "0": {point.setRedColor();} break;
             case "1":{point.setBlueColor();} break;
             default:{point.setGreyColor();} break;
         }
-        closeSocket();
+        //closeSocket();
     }
+
+//    private void closeSocket() {
+//        try {
+//            this.socket.close();
+//        } catch (IOException e) {
+//        }
+//    }
+
 }
--- laba5Server/src/RequestHandler.java
+++ laba5Server/src/RequestHandler.java
@@ -24,16 +24,13 @@ public class RequestHandler extends Thread {
             BufferedReader br = new BufferedReader(ir);
             ps = new PrintStream(client.getOutputStream());
             message = br.readLine();
-            System.out.println(message);
         } catch (IOException e) {
-            closeConnection();
             return;
         }
         float[] params = new float[3];//x,y,r
         String[] par = message.split(";");
         if (par.length!=3){
             System.err.println("Неверный формат сообщения");
-            closeConnection();
             return;
         }
         for (int i=0; i<3; i++){
@@ -42,25 +39,16 @@ public class RequestHandler extends Thread {
             }
             catch (NumberFormatException e){
                 System.err.println("Неверный формат сообщения");
-                closeConnection();
                 return;
             }
         }
 
 
         ps.print(new Kontur(params[2]).isInKontur(new Point(params[0], params[1])));
+        ps.flush();
         ps.close();
         System.out.println("Сообщение отправлено:   "+new Kontur(params[2]).isInKontur(new Point(params[0], params[1])));
-        closeConnection();
 
     }
 
-    void closeConnection(){
-
-        try {
-            this.client.close();
-        } catch (IOException e) {
-            System.err.println("Ошибка при закрытии соединения");
-        }
-    }
 }
