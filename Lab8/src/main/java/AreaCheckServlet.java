5350e45a557331a2a679329f11129b5557c90041
Александр Пашнин <panikun@yandex.ru>
2016-04-12 00:00:00 +0300

    Набросал основу JSP, пока без java кода и интерактивной области. Переделал Controller, удалил лишнего из AreaCheck

---
+++ Lab8/src/main/java/AreaCheckServlet.java
@@ -0,0 +1,40 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AreaCheckServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        String XBuf = req.getParameter("XSelector").replace(',', '.');
+        String YBuf = req.getParameter("YSelector").replace(',', '.');
+        String RBuf = req.getParameter("RSelector").replace(',', '.');
+        float X = Float.NaN;
+        float Y = Float.NaN;
+        float R = Float.NaN;
+        try {
+            X = Float.parseFloat(XBuf);
+            Y = Float.parseFloat(YBuf);
+            R = Float.parseFloat(RBuf);
+        } catch (NumberFormatException e) {
+        }
+        PrintWriter out = resp.getWriter();
+        if (!validate(X,Y,R)){
+            return;
+        }
+    }
+
+    boolean inFigure(float X, float Y, float R) {
+        return (Y >= 0 && Y <= R && X <= 0 && X >= R / 2) &&
+                (Y >= -R / 2 && Y <= 0 && X >= -R / 2 && X <= 0 && (X * X + Y * Y) >= R * R) &&
+                (Y >= -R / 2 && Y <= 0 && X >= 0 / 2 && X <= R && Y >= X / 2 - R / 2);
+    }
+
+    boolean validate(float X, float Y, float R) {
+        return (!Float.isNaN(X) && !Float.isNaN(Y) && !Float.isNaN(R) && R > 0 && X <= 5 && X >= -3 && Y <= 3 && Y >= -5);
+    }
+}
--- Lab8/src/main/java/ControllerServlet.java
+++ Lab8/src/main/java/ControllerServlet.java
@@ -1,4 +1,3 @@
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,12 +7,15 @@ import java.io.IOException;
 public class ControllerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/view").forward(req, resp);
+        if (req.getParameter("YSelector") == null || req.getParameter("XSelector") == null || req.getParameter("RSelector") == null) {
+            req.getRequestDispatcher("/view.jsp").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("check").forward(req, resp);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/check");
-        dispatcher.forward(req, resp);
+        req.getRequestDispatcher("/view.jsp").forward(req, resp);
     }
 }
